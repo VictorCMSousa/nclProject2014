7ae7502dad45b7d9b0925d5530bc47339209a681
VictorDev <victorcm.sousa@gmail.com>
2014-05-11 12:26:26 -0300

    retorno da votação exemplos no readme

--- Api.php
+++ Api.php
@@ -1,5 +1,55 @@
 <?php
 
+define("TODOS_JOGADORES", 0);
+define("RANKING_VOTES", 1);
+function _FetchJogadores($result, $mode)
+{	
+	$cont = 0;
+	print("jogadores = { \n");
+	if($mode == 0 )
+	{
+		while($row = mysqli_fetch_array($result)) 
+		{
+			//echo $row['nome'];
+			if($cont == 0)
+			{
+				print("{ \n");
+				$cont++;
+			}
+			else
+				print(",{ \n");
+			print(" id = " . $row['id'] . ", \n");
+			print(" name = " . $row['nome'] . ", \n");
+			print(" status = " .$row['status'] .",  \n");
+			print(" time =" . $row['time']."  \n");
+			print("} \n");		
+		}
+	}
+	else
+	{
+		while($row = mysqli_fetch_array($result)) 
+		{
+			//echo $row['nome'];
+			if($cont == 0)
+			{
+				print("{ \n");
+				$cont++;
+			}
+			else
+				print(",{ \n");
+			print(" nome_entra = " . $row['nome_entra'] . ", \n");
+			print(" time_entra = " . $row['time_entra'] . ", \n");
+			print(" camisa_entra = " .$row['camisa_entra'] .",  \n");
+			print(" nome_sai = " . $row['nome_sai'] . ", \n");
+			print(" time_sai = " . $row['time_sai'] . ", \n");
+			print(" camisa_sai = " .$row['camisa_sai'] ."  \n");
+			print("} \n");		
+		}
+	}
+	print("}\n");  
+	
+}
+
 function GetJogadores()
 {
 	$con=mysqli_connect("127.0.0.1","root","","redesmm");
@@ -9,49 +59,38 @@ function GetJogadores()
 
 	//mysqli_query($con,"INSERT INTO substituicao (id_jog_entra, id_jog_sai, id_partida)
 	//VALUES ('1', '12',1)");	
+	_FetchJogadores(mysqli_query($con,"SELECT * FROM jogador "), TODOS_JOGADORES);
 	
-	$result = mysqli_query($con,"SELECT * FROM jogador");
-	$cont = 0;
-	print("jogadores = { \n");
-	while($row = mysqli_fetch_array($result)) 
-	{
-		//echo $row['nome'];
-		if($cont == 0)
-		{
-			print("{ \n");
-			$cont++;
-		}
-		else
-			print(",{ \n");
-		print(" id = " . $row['id'] . ", \n");
-		print(" name = " . $row['nome'] . ", \n");
-		print(" status = " .$row['status'] .",  \n");
-		print(" time =" . $row['time']."  \n");
-		print("} \n");		
-	}
-	print("}\n");  
 	mysqli_close($con);
 	//echo "ola";
 }
 
-function SetVotes($id_jog_entra, $id_jog_sai)
+function SetVotes($id_jog_entra, $id_jog_sai, $time)
 {
+
 	$con=mysqli_connect("127.0.0.1","root","","redesmm");
 	// Check connection
 	if (mysqli_connect_errno()) {
-	  echo "Failed to connect to MySQL: " . mysqli_connect_error();	}
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();	
+	  }
 
 	mysqli_query($con,"INSERT INTO substituicao (id_jog_entra, id_jog_sai, id_partida)
 	VALUES ($id_jog_entra, $id_jog_sai,1)");
-	
+
+	//mysqli_query($con,"SELECT *, Count(id) soma FROM `substituicao` WHERE id_partida = 1 group by id_jog_entra, id_jog_sai order by soma desc limit 1");	
+	_FetchJogadores(mysqli_query($con,"SELECT jog.nome as 'nome_entra', jog.time as 'time_entra', jog.camisa as 'camisa_entra',jog2.nome as 'nome_sai', 
+jog2.time as 'time_sai', jog2.camisa as 'camisa_sai',Count(sub.id) soma 
+FROM `substituicao` as sub inner join `jogador` as jog on jog.id = sub.id_jog_entra inner join `jogador` as jog2 on jog2.id = sub.id_jog_sai WHERE id_partida = 1 group by id_jog_entra, id_jog_sai order by soma desc limit 1"), RANKING_VOTES);	
+
 	mysqli_close($con);
 	
 }
-	//SetVotes(1,2);
+	SetVotes(1,2, "Brasil");
+	
 	//GetJogadores();
 	if(isset($_REQUEST["jogadores"])) 
 	{
-		GetJogadores();
+		GetJogadores("");
 	}
 	
 	if(isset($_REQUEST["voto"])) 
@@ -59,8 +98,9 @@ function SetVotes($id_jog_entra, $id_jog_sai)
 		//voto = strtolower($_REQUEST["voto"]);
 		$id_jog_entra = $_REQUEST["id_jog_entra"];
 		$id_jog_sai = $_REQUEST["id_jog_entra"];
+		$time = $_REQUEST["time"];
 		
-		SetVotes($id_jog_entra, $id_jog_sai);
+		SetVotes($id_jog_entra, $id_jog_sai, $time);
 	}
 	
 ?>
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 nclProject2014
 ==============
+
+
+API
+
+Para pegar todos os jogadores fazer Request ["jogadores"]
+Exemplo de retorno:
+jogadores = { { id = 1, name = Julio Cesar, status = titular, time = Brasil } ,{ id = 2, name = Julio Cesar, status = titular, time = Brasil } ,{ id = 3, name = Jefferson, status = reserva, time = Brasil } ,{ id = 4, name = Daniel Alves, status = titular, time = Brasil } ,{ id = 5, name = Dante, status = titular, time = Brasil } ,{ id = 6, name = David Luiz, status = titular, time = Brasil } ,{ id = 7, name = Marcelo, status = titular, time = Brasil } ,{ id = 8, name = Paulinho, status = titular, time = Brasil } ,{ id = 9, name = Hernanes, status = titular, time = Brasil } ,{ id = 10, name = Oscar, status = titular, time = Brasil } ,{ id = 11, name = Ramires, status = titular, time = Brasil } ,{ id = 12, name = Neymar, status = titular, time = Brasil } ,{ id = 13, name = Fred, status = titular, time = Brasil } ,{ id = 14, name = Hulk, status = reserva, time = Brasil } ,{ id = 15, name = Bernard, status = reserva, time = Brasil } ,{ id = 16, name = Willian, status = reserva, time = Brasil } ,{ id = 17, name = Luiz Gustavo, status = reserva, time = Brasil } ,{ id = 18, name = Maicon, status = reserva, time = Brasil } ,{ id = 19, name = Maxwell, status = reserva, time = Brasil } ,{ id = 20, name = Thiago Silva, status = reserva, time = Brasil } ,{ id = 21, name = Henrique, status = reserva, time = Brasil } ,{ id = 22, name = Romero, status = titular, time = Argentina } ,{ id = 23, name = Zabaleta, status = titular, time = Argentina } ,{ id = 24, name = Basanta, status = titular, time = Argentina } ,{ id = 25, name = Fernandez, status = titular, time = Argentina } ,{ id = 26, name = Rojo, status = titular, time = Argentina } ,{ id = 27, name = Mascherano, status = titular, time = Argentina } ,{ id = 28, name = Gago, status = titular, time = Argentina } ,{ id = 29, name = di Maria, status = titular, time = Argentina } ,{ id = 30, name = Messi, status = titular, time = Argentina } ,{ id = 31, name = Aguero, status = titular, time = Argentina } ,{ id = 32, name = Higuain, status = titular, time = Argentina } ,{ id = 33, name = Biglia, status = titular, time = Argentina } ,{ id = 34, name = Palacio, status = reserva, time = Argentina } ,{ id = 35, name = Lavezzi, status = reserva, time = Argentina } ,{ id = 36, name = Rodriguez, status = reserva, time = Argentina } }
+
+
+Para fazer votação request ["voto"] passando os seguintes parametros
+
+$_REQUEST["id_jog_entra"];
+$_REQUEST["id_jog_entra"];
+$_REQUEST["time"];
+
+Será retornado o mais votado seguindo o seguinte formato
+jogadores = { { nome_entra = Julio Cesar, time_entra = Brasil, camisa_entra = 1, nome_sai = Julio Cesar, time_sai = Brasil, camisa_sai = 1 } }
+
