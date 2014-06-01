ec6edd47b1e9cc87fe8b099f10a334f8a8f0c103
VictorDev <victorcm.sousa@gmail.com>
2014-06-01 19:04:56 -0300

    toma aaa

--- bd/jogador.sql
+++ bd/jogador.sql
@@ -41,29 +41,29 @@ CREATE TABLE IF NOT EXISTS `jogador` (
 --
 
 INSERT INTO `jogador` (`id`, `nome`, `camisa`, `status`, `time`, `foto_path`) VALUES
-(1, 'Julio Cesar', '1', 'titular', 'Brasil', './media/julio_cesar.jpg'),
-(3, 'Lucio', '3', 'titular', 'Brasil', './media/lucio.jpg'),
-(4, 'Daniel Alves', '13', 'titular', 'Brasil', './media/daniel_alves.jpg'),
-(5, 'Juan', '4', 'titular', 'Brasil', './media/juan.jpg'),
-(6, 'Maicon', '2', 'titular', 'Brasil', './media/maicon.jpg'),
-(7, 'Gilberto Silva', '8', 'titular', 'Brasil', './media/gilberto_silva.jpg'),
-(8, 'Elano', '7', 'titular', 'Brasil', './media/elano.jpg'),
-(9, 'Felipe Melo', '5', 'titular', 'Brasil', './media/felipe_melo.jpg'),
-(10, 'Kaka', '10', 'titular', 'Brasil', './media/kaka.jpg'),
-(11, 'Robinho', '11', 'titular', 'Brasil', './media/robinho.jpg'),
-(12, 'Luis Fabiano', '9', 'titular', 'Brasil', './media/luis_fabiano.jpg'),
-(13, 'Gomes', '12', 'reserva', 'Brasil', './media/gomes.jpg'),
-(14, 'Thiago Silva', '15', 'reserva', 'Brasil', './media/thiago_silva.jpg'),
-(15, 'Luisao', '14', 'reserva', 'Brasil', './media/luisao.jpg'),
-(16, 'Gilberto', '16', 'reserva', 'Brasil', './media/gilberto.jpg'),
-(17, 'Ramires', '18', 'reserva', 'Brasil', './media/ramires.jpg'),
-(18, 'Kleberson', '20', 'reserva', 'Brasil', './media/kleberson.jpg'),
-(19, 'Josue', '17', 'reserva', 'Brasil', './media/josue.jpg'),
-(20, 'Grafite', '23', 'reserva', 'Brasil', './media/grafite.jpg'),
-(21, 'Nilmar', '21', 'reserva', 'Brasil', './media/nilmar.jpg'),
-(22, 'Doni', '22', 'reserva', 'Brasil', './media/doni.jpg'),
-(23, 'Michel Bastos', '6', 'reserva', 'Brasil', './media/michel_bastos.jpg');
-(23, 'Julio Baptista', '35', 'reserva', 'Brasil', './media/julio_baptista.jpg');
+(1, 'Julio Cesar', '1', 'titular', 'Brasil', 'media/julio_cesar.jpg'),
+(3, 'Lucio', '3', 'titular', 'Brasil', 'media/lucio.jpg'),
+(4, 'Daniel Alves', '13', 'titular', 'Brasil', 'media/daniel_alves.jpg'),
+(5, 'Juan', '4', 'titular', 'Brasil', 'media/juan.jpg'),
+(6, 'Maicon', '2', 'titular', 'Brasil', 'media/maicon.jpg'),
+(7, 'Gilberto Silva', '8', 'titular', 'Brasil', 'media/gilberto_silva.jpg'),
+(8, 'Elano', '7', 'titular', 'Brasil', 'media/elano.jpg'),
+(9, 'Felipe Melo', '5', 'titular', 'Brasil', 'media/felipe_melo.jpg'),
+(10, 'Kaka', '10', 'titular', 'Brasil', 'media/kaka.jpg'),
+(11, 'Robinho', '11', 'titular', 'Brasil', 'media/robinho.jpg'),
+(12, 'Luis Fabiano', '9', 'titular', 'Brasil', 'media/luis_fabiano.jpg'),
+(13, 'Gomes', '12', 'reserva', 'Brasil', 'media/gomes.jpg'),
+(14, 'Thiago Silva', '15', 'reserva', 'Brasil', 'media/thiago_silva.jpg'),
+(15, 'Luisao', '14', 'reserva', 'Brasil', 'media/luisao.jpg'),
+(16, 'Gilberto', '16', 'reserva', 'Brasil', 'media/gilberto.jpg'),
+(17, 'Ramires', '18', 'reserva', 'Brasil', 'media/ramires.jpg'),
+(18, 'Kleberson', '20', 'reserva', 'Brasil', 'media/kleberson.jpg'),
+(19, 'Josue', '17', 'reserva', 'Brasil', 'media/josue.jpg'),
+(20, 'Grafite', '23', 'reserva', 'Brasil', 'media/grafite.jpg'),
+(21, 'Nilmar', '21', 'reserva', 'Brasil', 'media/nilmar.jpg'),
+(22, 'Doni', '22', 'reserva', 'Brasil', 'media/doni.jpg'),
+(23, 'Michel Bastos', '6', 'reserva', 'Brasil', 'media/michel_bastos.jpg');
+(24, 'Julio Baptista', '35', 'reserva', 'Brasil', 'media/julio_baptista.jpg');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
